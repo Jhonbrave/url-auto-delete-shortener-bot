2deabeb60aeb44213e86d38e415232760e6352cb
Codder <122482673+Jhonbrave@users.noreply.github.com>
2023-01-18 14:26:07 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -84,7 +84,7 @@ async def start(client, message):
                 btn.append([InlineKeyboardButton(" 🔄 Try Again", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
         await client.send_message(
             chat_id=message.from_user.id,
-            text="**𝑱𝒐𝒊𝒏 𝑶𝒖𝒓 𝑴𝒐𝒗𝒊𝒆 𝑼𝒑𝒅𝒂𝒕𝒆𝒔 𝑪𝒉𝒂𝒏𝒏𝒆𝒍 𝑻𝒐 𝑼𝒔𝒆 𝑻𝒉𝒊𝒔 𝑩𝒐𝒕!**",
+            text="**NEW USERS MUST JOIN THE CHANNEL BELOW TO GET YOUR MOVIE \n THEN COME BACK HERE AND TAP ON --TRY AGAIN-- U r movie will come **",
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode=enums.ParseMode.MARKDOWN
             )
